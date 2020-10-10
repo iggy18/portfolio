726a03b2cc3ccd384d92ffe0c3df3c50ca204bef
seth mcfeeters <seth.mcfeeters@gmail.com>
2020-10-10 15:43:55 -0700

    fixed it

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./public'));
+app.use(express.static('./public/public'));
 
 app.get('/hello', (request, response) => {
   response.status(200).send('Hello');
@@ -21,6 +21,6 @@ app.get('/data', (request, response) => {
   response.status(200).json(airplanes);
 });
 
-// app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
